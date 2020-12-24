1ddc48862c733b1aaf85d2b6cd6c80c071524f1f
Vadim <pnevmat001@gmail.com>
2020-12-25 00:26:38 +0200

    fixes bugs with timer count

--- src/js/timer-counter.js
+++ src/js/timer-counter.js
@@ -4,18 +4,19 @@
 //     selector: '#timer-1',
 //     targetDate: new Date('Jul 17, 2021'),
 // }), {};
+const timerRef = document.querySelector('#timer-1');
+console.dir(timerRef);
 
 setInterval(() => {
-    const timerRef = document.querySelector('#timer-1');
     const time = new Date('Jul 17, 2021') - Date.now();
     const days = Math.floor(time / (1000 * 60 * 60 * 24));
     const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
     const secs = Math.floor((time % (1000 * 60)) / 1000);
-    timerRef.childNodes[2].childNodes[1].innerHTML = days;
-    timerRef.childNodes[4].childNodes[1].innerHTML = hours;
-    timerRef.childNodes[6].childNodes[1].innerHTML = mins;
-    timerRef.childNodes[8].childNodes[1].innerHTML = secs;
+    timerRef.children[0].children[0].innerHTML = days;
+    timerRef.children[1].children[0].innerHTML = hours;
+    timerRef.children[2].children[0].innerHTML = mins;
+    timerRef.children[3].children[0].innerHTML = secs;
 }, 1000);
 
 
