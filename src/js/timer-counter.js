040382a3f84440cedba025b81ee0c65eb41bfb80
Vadim <pnevmat001@gmail.com>
2021-01-16 15:35:57 +0200

    Fixes timer timeout and class call

--- src/js/timer-counter.js
+++ src/js/timer-counter.js
@@ -7,20 +7,7 @@ import CountdownTimer from './timerClass'
     targetDate: new Date('Jul 17, 2021'),
 });
 
-const timerRef = document.querySelector('#timer-1');
-console.dir(timerRef);
-
-setInterval(() => {
-    const time = new Date('Jul 17, 2021') - Date.now();
-    const days = Math.floor(time / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-    const secs = Math.floor((time % (1000 * 60)) / 1000);
-    timerRef.children[0].children[0].innerHTML = days;
-    timerRef.children[1].children[0].innerHTML = hours;
-    timerRef.children[2].children[0].innerHTML = mins;
-    timerRef.children[3].children[0].innerHTML = secs;
-}, 1000);
+timer.timerStart();
 
 
 
